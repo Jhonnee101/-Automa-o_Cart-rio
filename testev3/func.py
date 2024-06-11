d1e3d1553906d2af328ba1a4b30f6de98f96429b
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2024-06-10 23:03:03 -0300

    att

--- testev3/func.py
+++ testev3/func.py
@@ -2,6 +2,7 @@ from tkinter import messagebox
 from tkinter import filedialog
 import openpyxl as xl
 import docx
+from docx.shared import Pt
 
 caminho_entrada = ""
 caminho_saida = ""
@@ -21,11 +22,15 @@ def extrair_matriculas():
     workbook = xl.load_workbook(caminho_entrada)
     planilha = workbook.active
 
+    doc = docx.Document()
+    doc.styles['Normal'].font.name = 'Courier New'
+    doc.styles['Normal'].font.size = Pt(12)
+    doc.styles['Normal'].paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
+
     for rows in planilha.iter_rows(min_row=2):
         mat = rows[0].value
         texto = rows[1].value
 
-        doc = docx.Document()
         doc.add_paragraph(f"Matrícula: {texto}")
 
         nome_arquivo = f"{mat} FALTA CONFERIR.docx"
