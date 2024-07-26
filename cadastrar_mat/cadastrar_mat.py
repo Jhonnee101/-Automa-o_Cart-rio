b5f8e87038585d5664acb12fecce9984f8f874f3
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2024-07-26 20:07:56 -0300

    Adicionado os Requisitos de automação

--- cadastrar_mat/cadastrar_mat.py
+++ cadastrar_mat/cadastrar_mat.py
@@ -1,2 +1,15 @@
-print("Hello Word")
-a = 2+1
+#Cadastrar apontamentos 
+#Ler planilha contendo os dados para ser preenchidos
+#Clicar em matriculas
+#Clicar em apontamentos
+#Clicar em cadastrar apontamentos antigo
+#Clicar e digitar o nº da matricula a ser cadastrada
+#Selecionar se é averbação ou não
+#Clicar e digitar o nº do apontamento
+#Clicar em cadastrar apontamento
+#Inserir a data
+#Colar texto da matricula + Ctrl + A
+#Clicar em justificar
+#Selecionar fonte courier new
+#Selecionar tamanho 16
+#Clicar em salvar
