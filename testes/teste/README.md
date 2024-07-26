a8722d8c725cc488f4274802656d28d2d14516d8
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2024-07-26 20:10:45 -0300

    Delete testes directory

--- testes/teste/README.md
+++
@@ -1,1 +0,0 @@
-#  Automação_Cartório
--- testes/teste/test1.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import filedialog
-
-root = tk.Tk()
-caminho_arquivo = filedialog.asksaveasfilename(defaultextension=".txt")
-if caminho_arquivo:
-    with open(caminho_arquivo, "w") as arquivo:
-        arquivo.write("Conteúdo do arquivo") 
